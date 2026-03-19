8211980f1b9044299991db8f4a8bb8173bfe769e
Salfi <zz3997963@gmail.com>
2026-03-20 07:07:23 +0800

    fix frontend script loading rewrite path

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
       { source: '/@vite/client', destination: '/vite-client-shim.js' },
       { source: '/@vite/:path*', destination: '/vite-client-shim.js' },
       // Map custom asset prefix back to Next's internal _next path
-      { source: '/assets/:path*', destination: '/_next/:path*' },
+      { source: '/assets/:path*', destination: '/:path*' },
     ]
   },
 }
